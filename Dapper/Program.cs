ab3809a3d0d1f19e5959679fbb5ab4b936740711
ifirebird <ifirebird@hotmail.com>
2019-05-09 11:28:59 +0800

    初始提交项目文件

---
+++ Dapper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(new crud().GetemployeeListFirst().FirstOrDefault().displayname);
+            List<t_department> list = new crud().GetPCEntity().ToList();
+            Console.Read();
+        }
+    }
+}
